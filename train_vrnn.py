8d2f2a5a2de9b861c811c46b9ad48652c43c8611
Thomas McColgan <thomas.mccolgan@gmail.com>
2016-11-07 10:19:20 +0100

    clean up number of batches

--- train_vrnn.py
+++ train_vrnn.py
@@ -15,9 +15,10 @@ from matplotlib import pyplot as plt
 '''
 TODOS:
     - parameters for depth and width of hidden layers
-    - implement predict and sample functions
+    - implement predict function
     - separate binary and gaussian variables
     - clean up nomenclature to remove MDCT references
+    - implement separate MDCT training and sampling version
 '''
 
 def next_batch(args):
@@ -47,7 +48,7 @@ def train(args, model):
         cPickle.dump(args, f)
 
     ckpt = tf.train.get_checkpoint_state(dirname)
-
+    n_batches = 100
     with tf.Session() as sess:
         summary_writer = tf.train.SummaryWriter('logs/' + datetime.now().isoformat().replace(':', '-'), sess.graph)
         check = tf.add_check_numerics_ops()
@@ -61,21 +62,21 @@ def train(args, model):
         for e in xrange(args.num_epochs):
             sess.run(tf.assign(model.lr, args.learning_rate * (args.decay_rate ** e)))
             state = model.initial_state_c, model.initial_state_h
-            for b in xrange(100):
+            for b in xrange(n_batches):
                 x, y = next_batch(args)
                 feed = {model.input_data: x, model.target_data: y}
                 train_loss, _, cr, summary, sigma, mu, input, target= sess.run(
                         [model.cost, model.train_op, check, merged, model.sigma, model.mu, model.flat_input, model.target],
                                                              feed)
-                summary_writer.add_summary(summary, e * 100 + b)
-                if (e * 100 + b) % args.save_every == 0 and ((e * 100 + b) > 0):
+                summary_writer.add_summary(summary, e * n_batches + b)
+                if (e * n_batches + b) % args.save_every == 0 and ((e * n_batches + b) > 0):
                     checkpoint_path = os.path.join(dirname, 'model.ckpt')
-                    saver.save(sess, checkpoint_path, global_step=e * 100 + b)
+                    saver.save(sess, checkpoint_path, global_step=e * n_batches + b)
                     print "model saved to {}".format(checkpoint_path)
                 end = time.time()
                 print "{}/{} (epoch {}), train_loss = {:.6f}, time/batch = {:.1f}, std = {:.3f}" \
-                    .format(e * 100 + b,
-                            args.num_epochs * 100,
+                    .format(e * n_batches + b,
+                            args.num_epochs * n_batches,
                             e, args.chunk_samples * train_loss, end - start, sigma.mean(axis=0).mean(axis=0))
                 start = time.time()
 
